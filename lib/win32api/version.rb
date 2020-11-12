60dfb502b762e937472e943854923d6ff1fcc7ee
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2020-11-12 11:01:36 +0900

    bundle gem win32api

---
+++ lib/win32api/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Win32api
+  VERSION = "0.1.0"
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "win32api"
+
+require "minitest/autorun"
